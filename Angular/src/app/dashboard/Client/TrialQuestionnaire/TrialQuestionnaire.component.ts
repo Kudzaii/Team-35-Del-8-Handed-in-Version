04f889b19298feb756ca434c21e8108d56ab491a
Kudzaii <u18055062@tuks.co.za>
2021-10-27 16:30:59 +0200

    Timer and trial questionnaire

--- Angular/src/app/dashboard/Client/TrialQuestionnaire/TrialQuestionnaire.component.ts
+++ Angular/src/app/dashboard/Client/TrialQuestionnaire/TrialQuestionnaire.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit, TemplateRef } from '@angular/core';
 import { Question } from 'src/app/models/Question';
 import { ClientService } from '../services/client.service';
 import { Location } from '@angular/common'
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+
 @Component({
   selector: 'app-TrialQuestionnaire',
   templateUrl: './TrialQuestionnaire.component.html',
@@ -10,7 +13,9 @@ import { Location } from '@angular/common'
 export class TrialQuestionnaireComponent implements OnInit {
 
   Questions: Array<Question>
-   constructor(private clientService: ClientService,private location: Location) { }
+  modalRef: BsModalRef | null;
+  modalRef2: BsModalRef;
+   constructor(private clientService: ClientService,private location: Location, private modalService: BsModalService) { }
 
   ngOnInit() {
     this.getQuestionsTrialQuestions()
@@ -27,4 +32,23 @@ export class TrialQuestionnaireComponent implements OnInit {
   goBack(): void {
     this.location.back();
   }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template, { id: 1, class: 'modal-lg' });
+  }
+  openModal2(template: TemplateRef<any>) {
+    this.modalRef2 = this.modalService.show(template, {id: 2, class: 'second' });
+  }
+  closeFirstModal() {
+    if (!this.modalRef) {
+      return;
+    }
+ 
+    this.modalRef.hide();
+    this.modalRef = null;
+  }
+  closeModal(modalId?: number){
+    this.modalService.hide(modalId);
+  }
+
 }
