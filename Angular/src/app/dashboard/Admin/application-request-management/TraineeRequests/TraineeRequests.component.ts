7611719ab98cf03b4ad9a2166cc5bab162720e73
TravisMcMean <62015955+TravisMcMean@users.noreply.github.com>
2021-11-02 11:23:06 +0200

    Updates

--- Angular/src/app/dashboard/Admin/application-request-management/TraineeRequests/TraineeRequests.component.ts
+++ Angular/src/app/dashboard/Admin/application-request-management/TraineeRequests/TraineeRequests.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 import { TraineesService } from '../../services/trainees.service';
 
 @Component({
@@ -16,11 +17,40 @@ export class TraineeRequestsComponent implements OnInit {
   }
 
   AcceptOrReject(trainee, decision){
-    
-    this.traineeservice.AcceptORejectTrainee(trainee,decision).subscribe(()=>{
-      this.refreshData()
+    let tempString
+    let resultPopupString
+    if(decision ==1)
+    {
+      tempString = 'Are You Sure You Want To Accept This Practitioner?';
+      resultPopupString =  "Successfully Accepted!"
+    }
+    else
+    {
+      tempString = 'Are You Sure You Want To Reject This Practitioner? If Yes Please Make Sure They Receive An Email As To Why They Have Been Rejected.';
+      resultPopupString =  "Successfully Rejected!"
+
+    }
+
+    Swal.fire({
+      title: tempString,
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: 'Save',
+      denyButtonText: `Don't Save`,
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        this.traineeservice.AcceptORejectTrainee(trainee,decision).subscribe(()=>{
+          this.refreshData()
+          Swal.fire(resultPopupString, '', 'info')
+        })
+      } else if (result.isDenied) {
+
+      }
     })
+
   }
+    
 
   refreshData(){
       this.traineeservice.getTraineeRegistrations().subscribe(res=>{
--- Angular/src/app/dashboard/Admin/application-request-management/TrainerRequests/TrainerRequests.component.ts
+++ Angular/src/app/dashboard/Admin/application-request-management/TrainerRequests/TrainerRequests.component.ts
@@ -16,11 +16,38 @@ export class TrainerRequestsComponent implements OnInit {
   }
 
   AcceptOrReject(trainee, decision){
+    let tempString
+    let resultPopupString
+    if(decision ==1)
+    {
+      tempString = 'Are You Sure You Want To Accept This Practitioner?';
+      resultPopupString =  "Successfully Accepted!"
+    }
+    else
+    {
+      tempString = 'Are You Sure You Want To Reject This Practitioner? If Yes Please Make Sure They Receive An Email As To Why They Have Been Rejected.';
+      resultPopupString =  "Successfully Rejected!"
 
+    }
 
-    this.trainerervice.AcceptORejectTrainer(trainee,decision).subscribe(()=>{
-      this.refreshData()
+    Swal.fire({
+      title: tempString,
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: 'Save',
+      denyButtonText: `Don't Save`,
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        this.trainerervice.AcceptORejectTrainer(trainee,decision).subscribe(()=>{
+          this.refreshData()
+          Swal.fire(resultPopupString, '', 'info')
+        })
+      } else if (result.isDenied) {
+
+      }
     })
+
   }
 
   refreshData(){
